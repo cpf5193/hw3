936491fff4d6da42c5ed8534797e32661d1c5f6e
cpf5193 <cpf5193@cs.washington.edu>
2013-11-06 21:04:46 -0800

    Re-formatted webpages

--- js/dawg-pizza.js
+++ js/dawg-pizza.js
@@ -1,10 +1,11 @@
-(window.onload = function(){
+$(function(){
 	//This function figures out which page is currently being displayed and
 	//distributes the active class to the appropriate nav section
 	var pageType = window.location.toString().split(".")[2].split('/')[4];
 	switch(pageType){
 	case "menu":
 	  $(".navbar-nav>li>a.menu")[0].parentNode.classList.add("active");
+	  populateMenu();
 	  break;
 	case "about":
 	   $(".navbar-nav>li>a.about")[0].parentNode.classList.add("active");
@@ -18,3 +19,16 @@
 	  break;
 	}
 });
+
+function populateMenu(){
+	var menuObj = com.dawgpizza.menu;
+	var template = $('.meat-pies .menu-item');
+	var pizzas = menuObj.pizzas;
+	for(var i=0; i<pizzas.length; ++i){
+		var temp = template.clone(), pizza = pizzas[i];
+		temp.find('.name').html(pizza.name);
+		console.log(temp);
+		$('.pizzas').append(temp);
+	}
+	console.log(menuObj);
+}
