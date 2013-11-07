7dd33305cadf78e1bd45eb649fba31984f728de2
cpf5193 <cpf5193@cs.washington.edu>
2013-11-06 21:21:51 -0800

    Ported local repo file to new folder

--- js/dawg-pizza.js
+++ js/dawg-pizza.js
@@ -22,13 +22,40 @@ $(function(){
 
 function populateMenu(){
 	var menuObj = com.dawgpizza.menu;
-	var template = $('.meat-pies .menu-item');
-	var pizzas = menuObj.pizzas;
-	for(var i=0; i<pizzas.length; ++i){
-		var temp = template.clone(), pizza = pizzas[i];
-		temp.find('.name').html(pizza.name);
-		console.log(temp);
-		$('.pizzas').append(temp);
+	var pizzas = menuObj.pizzas, drinks = menuObj.drinks,
+	desserts = menuObj.desserts;
+	buildMenuCategory(pizzas, $('.pizzas'));
+	buildMenuCategory(drinks, $('.drinks'));
+	buildMenuCategory(desserts, $('.desserts'));
+}
+
+function buildMenuCategory(menuType, parent){
+	console.log(menuType);
+	var template = parent.find('.menu-item');
+	for(var i=0; i<menuType.length; ++i){
+		var temp = template.clone(), menuItem = menuType[i];
+		var name = menuItem.name,
+			descr = menuItem.description,
+			price = menuItem.prices;
+		console.log(descr);
+		if(name){temp.find('.name').html(name);}
+		if(descr){temp.find('.descr').html(descr);}
+		if(price){temp.find('.price').html(makePriceString(price));}
+			else if(menuItem.price){
+				temp.find('.price').html('$' + menuItem.price);
+			}
+		temp.removeClass('disable');
+		parent.append(temp);
 	}
-	console.log(menuObj);
+}
+
+function makePriceString(array){
+	var string = "$";
+	for(var i=0; i<array.length-1; ++i){
+		string += (array[i] + "/$");
+	}
+	if(i>1){
+		string += array[i];
+	}
+	return string;
 }
